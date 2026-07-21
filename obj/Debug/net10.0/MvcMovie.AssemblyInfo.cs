cbd835329da23bb9e5a0011201a9c16334452b69
rdmmoonlight <ndopoer@gmail.com>
2026-07-21 10:10:28 +0700

    Bismillah, fix atau update

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11fc3920e8ef98993c87c62db9a32c9e219d0b4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8151848e9c71946910e16c7f78dee9350199b128")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
